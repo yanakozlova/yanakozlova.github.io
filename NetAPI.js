3620068277ff26f3f451dbdf275ee0b3da18a8c0
Yana Kozlova <yana.kozlova1989@gmail.com>
2018-02-06 00:22:10 +0200

    fix

--- NetAPI.js
+++ NetAPI.js
@@ -1,4 +1,4 @@
-let connection, connectionSpeed, htmlNode, htmlClass;
+let connection, connectionSpeed;
 connection = window.navigator.connection || {'type': '0'};
 
 switch (connection.type) {
@@ -15,16 +15,22 @@ switch (connection.type) {
         connectionSpeed = 'high';
 }
 console.log(connectionSpeed);
-const imgs = ['java.png', 'html5.png', 'qa.png', 'about_logo.png', 'Polygon.png', 'Polygon1.png' ];
+const imgs = ['java.png', 'html5.png', 'qa.png', 'about_logo.png', 'Polygon.png', 'Polygon1.png'];
 
-if (connectionSpeed === 'high' || connectionSpeed === 'medium') {
+if (connectionSpeed !== 'low') {
     imgs.forEach((item) => {
-        let img = document.createElement('img');
-        img.setAttribute('src','images/' + item);
-        const im_place = document.getElementsByClassName(item);
-        console.log(im_place);
-        im_place.forEach( (item)=> {item.appendChild(img)});
-    });
-
-
+            let img = document.createElement('img');
+            img.setAttribute('src', 'images/' + item);
+            const im_place = document.getElementsByClassName(item);
+            console.log(im_place.item(1));
+            if (im_place.length > 1) {
+                for (let i = 0; i < im_place.length) {
+                    im_place[i].appendChild(img)
+                }
+            } else {
+                im_place[0].appendChild(img)
+            }
+        }
+    );
 }
+
